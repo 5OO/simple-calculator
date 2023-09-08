6c91e208ecd7f8f27016109f1377627feb6cb122
5OO <git.hub@myrk.eu>
2023-09-08 16:21:55 +0300

    added method searchOperations in  ArithmeticOperationController.java class

--- src/main/java/com/po/simplecalculator/controller/ArithmeticOperationController.java
+++ src/main/java/com/po/simplecalculator/controller/ArithmeticOperationController.java
@@ -1,16 +1,37 @@
 package com.po.simplecalculator.controller;
 
 import com.po.simplecalculator.model.ArithmeticOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 public class ArithmeticOperationController {
 
+    @GetMapping("/add")
     public ArithmeticOperation addNumbers(@RequestParam("first") int first,  @RequestParam("second") int second) {
         if (first < 0 || first > 100 || second < 0 || second > 100) {
             throw new IllegalArgumentException("Arvud peavad olema vahemikus 0 kuni 100");}
         int sum = first + second;
         return new ArithmeticOperation(first, second, sum);
     }
+
+    private final List<ArithmeticOperation> operations = Collections.synchronizedList(new ArrayList<>());
+
+    @GetMapping("/search")
+    public List<ArithmeticOperation> searchOperations(
+            @RequestParam(required = false) Integer number,
+            @RequestParam("order") String order) {
+
+        // TODO  lisada otsimise  loogika
+
+
+        List<ArithmeticOperation> filteredAndOrderedOperations = null;
+        return filteredAndOrderedOperations;
+    }
+
 }
